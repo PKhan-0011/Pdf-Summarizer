057b47c9e536ad2f2f789b6b1a2ad9d8de5bca0a
PKhan-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-10-22 20:25:02 +0530

    chore: save current work before tailwind upgrade

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontsans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "pdf Summarizer",
+  description: "An app that summarize the pdf into a summary!..",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fontsans.variable} font-sans antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
