c0d9f7de1b5c3e9c1c63750a075da390b3acaa46
PKhan-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-10-29 13:57:03 +0530

    code push

--- src/home/HeroSection02.tsx
+++ src/home/HeroSection02.tsx
@@ -3,16 +3,25 @@ import { Pizza } from "lucide-react";
 
 const HeroSection02 = () => {
   return (
-    <div className="flex-col container mx-auto flex items-center justify-center gap-6">
-      <div>
-        <Pizza className="h-20 w-20 text-rose-600 " />
+    <div className="flex-col container mx-auto flex items-center justify-center gap-3 ">
+      <div className="inline-flex items-center justify-center p-2 rounded-2xl bg-gray-100 backdrop-blur-xs border border-gray-500/20 mb-4">
+        <Pizza className="h-10 w-10 text-rose-600 " />{" "}
+        {/* Bhai dekh yha p pizza ki jagah bhut sari chize tughe apne according set karni hai okkh! like pdf ya 2-3 etc etc.. */}
       </div>
 
       <div className="max-w-2xl">
         <h1 className="text-4xl font-extrabold tracking-normal">
           Watch How Sommarie transforms{" "}
-          <span className="text-rose-600">this Next.js course PDF</span> into an
-          easy-to-read <span className="pl-60">summary!</span>
+          <span className="bg-linear-to-r from-rose-500 to-rose-700 bg-clip-text text-transparent">
+            this Next.js course PDF
+          </span>{" "}
+          into an easy-to-read <span className="pl-60">summary!</span>
+        </h1>
+      </div>
+
+      <div>
+        <h1>
+          Yha p like ek crousel lagega okkh!.. Todha tagda wala hai wmo okkh!..
         </h1>
       </div>
     </div>
